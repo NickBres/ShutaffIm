75f1dcc1bb5ef5c5313d37079e22cb00d13be40e
yair489 <yairturgeman@gmail.com>
2024-01-23 13:29:33 +0200

    interested file and interested list

---
+++ app/src/main/java/com/example/shutaffim/InterstedView.kt
@@ -0,0 +1,115 @@
+package com.example.shutaffim
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+data class Person(
+    val images: List<Int>,
+    val name: String,
+    val phone: String,
+    val date: String
+)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InterstedItem(person: Person) {
+    val size = 70.dp
+
+    ElevatedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp,
+            focusedElevation = 256.dp
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.onSurface
+
+        ),
+        onClick = { /* TODO Enter to the item screen*/ }
+
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(size)
+        ) {
+            Image(
+                painter = painterResource(id = person.images[2]),
+                contentDescription = "",
+                modifier = Modifier
+                    .height(size)
+                    .width(size),
+                contentScale = ContentScale.Crop
+            )
+            Column() {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = person.name,
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                    )
+                    Text(
+                        text = person.date
+                    )
+
+
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        Icons.Default.Phone,
+                        contentDescription = "Location",
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+
+                    Text(
+                        text = person.phone,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        style = TextStyle(fontWeight = FontWeight.Bold)
+                    )
+                }
+            }
+
+
+        }
+    }
+
+}
