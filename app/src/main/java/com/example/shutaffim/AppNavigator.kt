b4c50b50b955a8d3d8addff0bf653348068ddec1
moshe <moshenh01@hmail.com>
2024-02-01 17:39:41 +0200

    add buttons and profile screen

--- app/src/main/java/com/example/shutaffim/AppNavigator.kt
+++ app/src/main/java/com/example/shutaffim/AppNavigator.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.shutaffim.Views.PostScreen
+import com.example.shutaffim.Data.Screen.PostScreen
 
 @Composable
 fun AppNavigator() {
