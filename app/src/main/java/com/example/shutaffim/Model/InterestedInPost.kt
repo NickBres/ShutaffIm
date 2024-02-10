d679b95a023346f3a17997f23ba2d31f490b737c
NickBres <70432147+NickBres@users.noreply.github.com>
2024-02-10 22:20:00 +0200

    minor

--- app/src/main/java/com/example/shutaffim/Model/InterestedInPost.kt
+++
@@ -1,8 +0,0 @@
-package com.example.shutaffim.Model
-
-data class InterestedInPost(
-    val postId: String,
-    val userEmail: String,
-    val message: String = "",
-    val isApproved: Boolean = false
-)
