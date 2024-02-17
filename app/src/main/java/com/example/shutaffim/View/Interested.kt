81f1b499d844056b97a2ebb640095efe1b6fc597
NickBres <70432147+NickBres@users.noreply.github.com>
2024-02-17 17:20:45 +0200

    interested item redesigned

--- app/src/main/java/com/example/shutaffim/View/Interested.kt
+++ app/src/main/java/com/example/shutaffim/View/Interested.kt
@@ -1,43 +1,29 @@
 package com.example.shutaffim
 
+//import com.example.shutaffim.Model.InterestedInPost
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.MutableLiveData
 import androidx.navigation.NavController
-//import com.example.shutaffim.Model.InterestedInPost
-import com.example.shutaffim.Model.Request
 import com.example.shutaffim.ViewModel.AuthViewModel
 import com.example.shutaffim.ViewModel.PostsVM
-import kotlinx.coroutines.launch
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Interested(
@@ -45,8 +31,6 @@ fun Interested(
     postsVM : PostsVM,
     userVM: AuthViewModel
 ) {
-
-
 
 
     val postImg = listOf(
@@ -59,7 +43,7 @@ fun Interested(
         mutableStateOf(false)
     }
 
-   val listOfInterested by postsVM.interestedInPost.observeAsState(initial = listOf())
+    val listOfInterested by postsVM.interestedInPost.observeAsState(initial = listOf())
     postsVM.getInterestedInPost(postsVM.currPost.value!!.id)
 
 
@@ -70,7 +54,7 @@ fun Interested(
             .padding(start = 4.dp, end = 4.dp),
     ) {
         items(listOfInterested) { request ->
-            InterestedItem(navController = navController, request = request, postsVM, userVM = userVM)
+            InterestedItem(request = request, userVM = userVM, postVm = postsVM)
         }
     }
 
--- app/src/main/java/com/example/shutaffim/View/InterstedItem.kt
+++ app/src/main/java/com/example/shutaffim/View/InterstedItem.kt
@@ -1,224 +1,206 @@
 package com.example.shutaffim
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.CardDefaults
+import androidx.compose.material.icons.filled.Message
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.shutaffim.Model.Request
 import com.example.shutaffim.ViewModel.AuthViewModel
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.LiveData
-import androidx.navigation.compose.rememberNavController
 import com.example.shutaffim.ViewModel.PostsVM
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import java.time.LocalDate
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
+
+//navController: NavController,
+//request: Request,
+//postVm: PostsVM,
+//userVM: AuthViewModel
 @Composable
-fun InterestedItem(navController: NavController,
-                   request: Request,
-                   postVm: PostsVM,
-                   userVM: AuthViewModel
+fun InterestedItem(
+    request: Request,
+    userVM: AuthViewModel,
+    postVm: PostsVM
 ) {
-    val size = 70.dp
-    val dummyPic = R.drawable.connor_jalbert_5b1mb7sdbg0_unsplash
-    var openMsgClick by remember {
-        mutableStateOf(false)
-    }
-
     userVM.getUserDataById(request.userId)
-    val userOfRequest by userVM.getUserFromId.observeAsState()
-    val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    val user by userVM.getUserFromId.observeAsState()
 
+    val userImage = rememberAsyncImagePainter(user!!.picture.pictureUrl)
+    val userName = user?.fName + " " + user?.lName
+    val currentYear = LocalDate.now().year
+    val birthYear = user?.birthYear ?: 0
+    val userSex = when (user?.sex) {
+        "Male" -> "♂"
+        "Female" -> "♀"
+        else -> "⚧"
+    }
+    val userInfo = "${currentYear - birthYear}, $userSex"
+    val userAbout = user?.about ?: ""
+    val userEmail = user?.email ?: ""
+    val message = request.message
+    var checked by remember { mutableStateOf(false) }
+    var showMessage by remember { mutableStateOf(false) }
+    var showUserAbout by remember { mutableStateOf(false) }
 
-    Box(
+    ElevatedCard(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(4.dp)
-             ){
-                ElevatedCard(
+            .wrapContentSize()
+            .padding(8.dp)
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(60.dp)
+                    .clickable {
+                        showUserAbout = !showUserAbout
+                        showMessage = false
+                    },
+                painter = userImage,
+                contentDescription = "user image",
+                contentScale = ContentScale.Crop
+            )
+            Column {
+                Text(
+                    text = userName,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    ),
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(4.dp),
-                    elevation = CardDefaults.cardElevation(
-                        defaultElevation = 4.dp,
-                        focusedElevation = 256.dp
+                        .padding(8.dp)
+                )
+                Text(
+                    text = userInfo,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
                     ),
-                    colors = CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surface,
-                        contentColor = MaterialTheme.colorScheme.onSurface
-                    ),
-                    onClick = { /* TODO open the msg of the user*/
-                        openMsgClick = !openMsgClick
+                    modifier = Modifier
+                        .padding(start = 8.dp, end = 8.dp, bottom = 8.dp)
+                )
 
-                    }
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(size)
-                    ) {
-                        Image(
-                            /*TODO: change the image to the user image*/
-                            painter = painterResource(dummyPic),
-                            contentDescription = "image of the user",
-                            modifier = Modifier
-                                .height(size)
-                                .width(size),
-                            contentScale = ContentScale.Crop
-                        )
-                        Column {
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(2.dp),
-                                horizontalArrangement = Arrangement.SpaceBetween,
-
-                                ) {
-                                Text(//fix alignment
-
-                                    text = (userOfRequest?.fName
-                                        ?: "user") + " " + (userOfRequest?.lName
-                                        ?: "user"),
-                                    style = TextStyle(
-                                        fontWeight = FontWeight.Bold,
-                                        fontSize = 16.sp
-                                    ),
-                                    modifier = Modifier.padding(4.dp)
-                                )
-
-
-                                Text(
-                                    text = sdf.format(Date(request.date)),
-                                    textAlign = TextAlign.Right,
-                                )
-                            }
-                            Spacer(modifier = Modifier.height(8.dp))
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(2.dp),
-
-                            ) {
-                                IconButton(
-                                    onClick = {
-                                        postVm.removeInterestedFromPost(
-                                            request.userId,
-                                            request.postId
-                                        )
-
-                                    }
-
-                                ) {
-                                    Icon(Icons.Default.Delete, contentDescription = "Delete")
-                                }
-                                Spacer(modifier = Modifier.width(186.dp))
-                                val checkedState = remember { mutableStateOf(false) }
-                                Checkbox(
-                                    checked = checkedState.value,
-                                    onCheckedChange = {
-                                        checkedState.value = it
-                                        if (checkedState.value) {
-                                            /* TODO: build func that update isApprove*/
-                                            postVm.updateIsApproved(
-                                                request.postId,
-                                                request.userId,
-                                                true
-                                            )
-                                        } else {
-                                            postVm.updateIsApproved(
-                                                request.postId,
-                                                request.userId,
-                                                false
-                                            )
-                                        }
-                                    },
-                                    modifier = Modifier
-                                        .fillMaxSize()
-
-                                    //place the checkbox in the end of the row
-
-                                )
-
-                            }
-
-                        }
-                    }
-                    AnimatedVisibility(visible = openMsgClick) {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                                .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.0f))
-                                .clickable {
-                                    userVM.insertCurrentInterested(request.userId)
-                                    navController.navigate(Screen.ProfileScreen.route)/* do something when clicked */ }
-                        ) {
-                            Text(
-                                text = request.message,
-                                style = TextStyle(
-                                    fontWeight = FontWeight.Bold,
-                                    fontSize = 16.sp
-                                ),
-                                modifier = Modifier.padding(4.dp)
-                            )
-                        }
-                    }
-                }
+            }
+            FloatingActionButton(
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(8.dp),
+                onClick = {
+                    showMessage = !showMessage
+                    showUserAbout = false
+                },
+                contentColor = MaterialTheme.colorScheme.surface,
+                containerColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Icon(Icons.Default.Message, contentDescription = "message")
             }
 
-}
+            Checkbox(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .align(Alignment.CenterVertically),
+                checked = checked,
+                onCheckedChange = {
+                    checked = it
+                    if (checked) {
+                        /* TODO: build func that update isApprove*/
+                        postVm.updateIsApproved(
+                            request.postId,
+                            request.userId,
+                            true
+                        )
+                    } else {
+                        postVm.updateIsApproved(
+                            request.postId,
+                            request.userId,
+                            false
+                        )
+                    }
+                }
+            )
+
+        }// row
+        AnimatedVisibility(visible = showMessage) {
+            Column {
 
 
-//preview
-@Composable
-@Preview(showBackground = true)
-fun InterestedItemPreview() {
-    val navController = rememberNavController()
-//    InterestedItem(
-//
-//        navController = navController, request = Request(
-//            "cons@gmail.com",
-//            "user1", "2021-10-10", false, "I'm interested in your item"
-//        )
-//    )
+                Text(
+                    text = message,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    ),
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+                Text(
+                    text = "To contact: $userEmail",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    ),
+                    modifier = Modifier
+                        .padding(8.dp)
+                )
+            }
+
+        }
+        AnimatedVisibility(visible = showUserAbout) {
+            Text(
+                text = userAbout,
+                style = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                ),
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+
+        }
+
+
+    }
+
+
 }
